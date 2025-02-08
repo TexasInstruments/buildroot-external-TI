2bfbdbcaefe3ac8e107165d9eee170fb7d768e9e
Paresh Bhagat <p-bhagat@ti.com>
2025-02-08 20:11:32 +0530

    Add support for AM62Lx-evm
    This commit adds support for AM62Lx-evm. Add config files for RT and non
    RT build for am62lx-evm. Add genimage_ti.cfg for output image layout.
    Add post build script to package dtb, kernel image and required
    firmwares in root partition. post-build script is also used to rename
    tiboot3-am62lx-hs-fs-evm.bin to tiboot3.bin for HSFS build. Add rootfs
    overlay to package files to target filesystem for weston, dropbear and
    network.
    
    Add required hashes corresponding to 11.00.02 tag.
    
    Signed-off-by: Paresh Bhagat <p-bhagat@ti.com>

---
+++ board/ti/am62lx-evm/post-build-ti.sh
@@ -0,0 +1,36 @@
+#!/bin/sh -x
+
+dtb_file=""
+
+while [ $# -gt 0 ]; do
+	case "$1" in
+		-d)
+			# Check if the next argument exists and is not another flag
+			if [ -n "$2" ] && [ "${2#-}" = "$2" ]; then
+				dtb_file="$2"
+				shift
+			else
+				echo "Error: Missing or invalid filename after -d flag"
+				exit 1
+			fi
+			;;
+		*)
+			;;
+	esac
+	shift
+done
+
+if [ -n "$dtb_file" ]; then
+	echo "dtb file provided : $dtb_file"
+else
+	echo "No dtb file provided with -d flag"
+	exit 1
+fi
+
+mkdir -p $TARGET_DIR/boot/dtb/ti
+mkdir -p $TARGET_DIR/lib/firmware/ti-connectivity
+
+cp $BINARIES_DIR/Image $TARGET_DIR/boot
+cp $BINARIES_DIR/$dtb_file $TARGET_DIR/boot/dtb/ti
+mv $BINARIES_DIR/tiboot3-am62lx-hs-fs-evm.bin $BINARIES_DIR/tiboot3.bin
+cp $BUILD_DIR/ti-k3-boot-firmware-*/ti-connectivity/* $TARGET_DIR/lib/firmware/ti-connectivity/
