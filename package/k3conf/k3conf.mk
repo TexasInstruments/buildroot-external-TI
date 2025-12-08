6b89238054c67068f2444914e706440fc65e3e08
sadik <s-sadik@ti.com>
2025-12-08 19:26:29 +0530

    package: update k3-conf version
    upadate k3-conf version and hash value for
    the latest v0.4 version.
    
    Signed-off-by: sadik <s-sadik@ti.com>

--- package/k3conf/k3conf.mk
+++ package/k3conf/k3conf.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-K3CONF_VERSION = origin/master
+K3CONF_VERSION = v0.4
 K3CONF_SITE = https://git.ti.com/git/k3conf/k3conf.git
 K3CONF_SITE_METHOD = git
 K3CONF_LICENSE = BSD-3-Clause
